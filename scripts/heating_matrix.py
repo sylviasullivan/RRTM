1ca187bf1702ac87202faad567738dc8c904ede2
sylviasullivan <sylvia.c.sullivan@gmail.com>
2020-06-24 17:27:29 +0200

    Tb, Tt, IWP sensitivity plot version 1

--- scripts/heating_matrix.py
+++ scripts/heating_matrix.py
@@ -19,9 +19,8 @@ g = 9.8
 #tt = np.asarray([196,197,199,200,202,204,205,207,209,210,212,214,215,217,\
 #        218,220,222,225,227,229,230,232,234,235,237,239,240,242,244,\
 #         245,247,249,251,252,254,256,257,260,261,262,264,266,267,269])
-tt = np.asarray([202,204,205,207,209,210,212,214,215,217,\
-        218,220,222,225,227,229,230,232,234,235,237,239,240,242,244,\
-         245,247,249,251,252,254,256,257,260,261])
+tt = np.arange(200,232)
+tb = np.arange(206,238)
 
 # Pressure at half levels
 ellingson = np.genfromtxt(basedir + '/output/tropical_profile_ellingson_250m_formatted_top2bottom.txt')
@@ -40,47 +39,64 @@ tropopause = np.argmin(tt_hl)
 fs = 13
 
 # Matrix to hold the radiative heating profiles
-H = np.zeros((4,tt.shape[0],lev+1))
-
-for c,tk in enumerate(tt):
-    lw1 = np.genfromtxt(basedir + '/output/q2_1lev/lwflxatm-test' + str(tk) + '_q2.txt')
-    sw1 = np.genfromtxt(basedir + '/output/q2_1lev/swflxatm-test' + str(tk) + '_q2.txt')
-    lw5 = np.genfromtxt(basedir + '/output/q2_5lev/lwflxatm-test' + str(tk) + '_q2.txt')
-    sw5 = np.genfromtxt(basedir + '/output/q2_5lev/swflxatm-test' + str(tk) + '_q2.txt')
+H = np.zeros((6,tt.shape[0],lev+1))
+c = 1
+for tk in tt:
+    lw_Tb  = np.genfromtxt(basedir + '/output/Tb/lwflxatm-Tb_' + str(tk+6) + '.txt')
+    sw_Tb  = np.genfromtxt(basedir + '/output/Tb/swflxatm-Tb_' + str(tk+6) + '.txt')
+    lw_Tt  = np.genfromtxt(basedir + '/output/Tt/lwflxatm-Tt_' + str(tk) + '.txt')
+    sw_Tt  = np.genfromtxt(basedir + '/output/Tt/swflxatm-Tt_' + str(tk) + '.txt')
+    if c < 10:
+       cstr = '0' + str(c)
+    else:
+       cstr = str(c)
+    lw_IWP = np.genfromtxt(basedir + '/output/THI/lwflxatm-IWP' + cstr + '_THI.txt')
+    sw_IWP = np.genfromtxt(basedir + '/output/THI/swflxatm-IWP' + cstr + '_THI.txt')
 
     # First column is all-sky. Second column is clear-sky.
     # The factor -1 is to define outgoing fluxes as positive.
-    lwcld1 = -1.*(lw1[0]-lw1[1])
-    swcld1 = -1.*(sw1[0]-sw1[1])
-    lwcld5 = -1.*(lw5[0]-lw5[1])
-    swcld5 = -1.*(sw5[0]-sw5[1])
+    lwcld_Tb  = -1.*(lw_Tb[0]-lw_Tb[1])
+    swcld_Tb  = -1.*(sw_Tb[0]-sw_Tb[1])
+    lwcld_Tt  = -1.*(lw_Tt[0]-lw_Tt[1])
+    swcld_Tt  = -1.*(sw_Tt[0]-sw_Tt[1])
+    lwcld_IWP = -1.*(lw_IWP[0]-lw_IWP[1])
+    swcld_IWP = -1.*(sw_IWP[0]-sw_IWP[1])
 
     # Calculate the longwave and shortwave heating rates.
     # Factor of 3600 converts from K s-1 to K day-1.
-    H[0,c] = g/cp*np.gradient(lwcld1,pp_hl*100.)*3600.
-    H[1,c] = g/cp*np.gradient(swcld1,pp_hl*100.)*3600.
-    H[2,c] = g/cp*np.gradient(lwcld5,pp_hl*100.)*3600.
-    H[3,c] = g/cp*np.gradient(swcld5,pp_hl*100.)*3600.
+    H[0,c-1] = g/cp*np.gradient(lwcld_Tb,pp_hl*100.)*3600.
+    H[1,c-1] = g/cp*np.gradient(swcld_Tb,pp_hl*100.)*3600.
+    H[2,c-1] = g/cp*np.gradient(lwcld_Tt,pp_hl*100.)*3600.
+    H[3,c-1] = g/cp*np.gradient(swcld_Tt,pp_hl*100.)*3600.
+    H[4,c-1] = g/cp*np.gradient(lwcld_IWP,pp_hl*100.)*3600.
+    H[5,c-1] = g/cp*np.gradient(swcld_IWP,pp_hl*100.)*3600.
 
-# Calculate the temperature depth of the various tropospheric layers.
-d1 = np.zeros(tt.shape[0])
-d5 = np.zeros(tt.shape[0])
-for k,t in enumerate(tt):
-    j = np.argmin(np.abs(tt_hl[tropopause:] - t))
-    # Take the difference of the midpoint 2-3 levels below and 2-3 levels above.
-    d5[k] = (tt_hl[tropopause+j+2] + tt_hl[tropopause+j+3])/2. - \
-            (tt_hl[tropopause+j-2] + tt_hl[tropopause+j-3])/2.
-    d1[k] = (tt_hl[tropopause+j] + tt_hl[tropopause+j+1])/2. - \
-            (tt_hl[tropopause+j] + tt_hl[tropopause+j-1])/2.
+    c = c + 1
 
-# Sensitivity of the LW/SW heating to the temperature depth.
-dHdT = np.zeros((2,H.shape[1],H.shape[2]))
-d5 = np.tile(d5,(H.shape[2],1)).T
-d1 = np.tile(d1,(H.shape[2],1)).T
+# Calculate the IWP perturbations between each of the H[4:5] values.
+IWP = np.loadtxt(basedir + '/output/IWP_iterator.txt')
+dIWP = np.zeros(len(IWP)-1)
+for j,iwp in enumerate(IWP):
+    if j == 0:
+       continue
+    else:
+       dIWP[j-1] = IWP[j] - IWP[j-1]
 
-# The factor of 6 is an approximate lapse rate to have sensitivity per km depth.
-dHdT[0] = (H[2] - H[0])/(d5 - d1)*6.
-dHdT[1] = (H[3] - H[1])/(d5 - d1)*6.
+# Sensitivity of the LW/SW heating to Tt/Tb (1-K perturbations).
+dHdT = np.zeros((6,H.shape[1]-1,H.shape[2]))
+for j in np.arange(H.shape[1]-1):
+    dHdT[:4,j] = (H[:4,j+1] - H[:4,j])
+    dHdT[4:,j] = (H[4:,j+1] - H[4:,j])/dIWP[j]
+    print(H[0,j+1])
+    print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+    print(H[0,j])
+    print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+    time.sleep(5) 
+
+#np.savetxt('dHdT_LW_Tb.txt',dHdT[0,:,tropopause+3:49])
+
+for i in np.arange(6):
+    print(np.nanmin(dHdT[i]),np.nanmax(dHdT[i]))
 
 fig, ax = plt.subplots(nrows=3,ncols=2,figsize=(11,11))
 # Some formatting factors for the plot.. fontsize, tick intervals, and iterator.
@@ -88,19 +104,20 @@ fs = 13
 interval = 5
 interval2 = 8
 c = 0
-mm = np.array([[-1.1,1.7],[-0.11,0.17],[-1.1,1.7],[-0.11,0.17]])
-ticz = np.array([[-1,-0.1,0,0.1,1],[-0.1,-0.01,0,0.01,0.1],[-1,-0.1,0,0.1,1],[-0.1,-0.01,0,0.01,0.1]])
+mm = np.array([[-1.1,1.1],[-0.1,0.1],[-1.1,1.1],[-0.1,0.1],[-2.5,7.5],[-5,7.5]])
+#ticz = np.array([[-1,-0.1,0,0.1,1],[-0.1,-0.01,0,0.01,0.1],[-1,-0.1,0,0.1,1],[-0.1,-0.01,0,0.01,0.1]])
 lwsw = ['LW','SW']
+lbl = [r'K day$^{-1}$ K$^{-1}$',r'K day$^{-1}$ K$^{-1}$',r'K day$^{-1}$ K$^{-1}$',\
+       r'K day$^{-1}$ K$^{-1}$',r'K day$^{-1}$ per g kg$^{-1}$',r'K day$^{-1}$ per g kg$^{-1}$']
 
 yt_tt_hl = np.array([int(t) for t in tt_hl[::interval2]])
 yt_pp_hl = np.array([int(t) for t in pp_hl[::interval]])
 xt_tt = np.array([int(t) for t in tt[::interval]])
 
-for i in np.arange(2):
+for i in np.arange(3):
     for j in np.arange(2):
-        sns.heatmap((H[c].T),norm=colors.SymLogNorm(vmin=mm[c,0],vmax=mm[c,1],linthresh=0.001),
-            cmap=cm.bwr,xticklabels=xt_tt,yticklabels=yt_tt_hl,cbar_kws={'label':r'K day$^{-1}$','ticks':ticz[c]},ax=ax[i,j])
-
+        sns.heatmap(dHdT[c].T,cmap=cm.bwr,ax=ax[i,j],norm=MidpointNormalize(midpoint=0.,vmin=mm[c,0],vmax=mm[c,1]),\
+                   xticklabels=xt_tt,yticklabels=yt_tt_hl,cbar_kws={'label':lbl[c]})
         # Where is the melting layer?
         ax[i,j].plot([0,tt.shape[0]],[melting_layer,melting_layer],linewidth=1,color='k',linestyle='--')
         # Where is the tropopause?
@@ -109,23 +126,10 @@ for i in np.arange(2):
         ax[i,j].set_xticks(np.arange(0,tt.shape[0],interval))
         ax[i,j].set_xticklabels(ax[i,j].get_xticklabels(),rotation=45,fontsize=fs-2)
         ax[i,j].set_yticks(np.arange(0,tt_hl.shape[0],interval2))
-        ax[i,j].set_yticklabels(ax[i,j].get_yticklabels(),rotation=45,fontsize=fs-2)
-        if i == 1:
-           ax[i,j].set_ylabel('Level of ' + lwsw[j] + ' heating perturbation [K]',fontsize=fs)
-        c += 1
+        ax[i,j].set_yticklabels(ax[i,j].get_yticklabels(),rotation=45,fontsize=fs-2) 
+        ax[i,j].set_ylim([49,tropopause+3]) # ylim is done based on indices not values.
+        ax[i,j].set_xlabel('Level of $q_i$ perturbation [K]',fontsize=fs)
+        c = c + 1
 
-for i in np.arange(2):
-    sns.heatmap(dHdT[i].T,cmap=cm.bwr,xticklabels=xt_tt,yticklabels=yt_tt_hl,cbar_kws={'label':r'K day$^{-1}$ km$^{-1}$'},ax=ax[2,i])
-    # Where is the melting layer?
-    ax[2,i].plot([0,tt.shape[0]],[melting_layer,melting_layer],linewidth=1,color='k',linestyle='--')
-    # Where is the tropopause?
-    ax[2,i].plot([0,tt.shape[0]],[tropopause,tropopause],linewidth=1,color='k',linestyle='--')
-    # Adjust tick labels
-    ax[2,i].set_xticks(np.arange(0,tt.shape[0],interval))
-    ax[2,i].set_xticklabels(ax[i,j].get_xticklabels(),rotation=45,fontsize=fs-2)
-    ax[2,i].set_yticks(np.arange(0,tt_hl.shape[0],interval2))
-    ax[2,i].set_yticklabels(ax[i,j].get_yticklabels(),rotation=45,fontsize=fs-2)
-    ax[2,i].set_xlabel('Level of $q_i$ perturbation [K]',fontsize=fs)
-
-fig.savefig('../figures/heating_matrix_qi2.pdf',bbox_inches='tight')
+#fig.savefig('../figures/heating_matrix_qi2.pdf',bbox_inches='tight')
 plt.show()
